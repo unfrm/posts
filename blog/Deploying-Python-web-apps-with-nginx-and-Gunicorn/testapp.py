6bfedf236647fc60366b78664ac2231c36d9b7bc
Dennis <hi@unfrm.us>
2016-07-17 18:42:12 +0200

    Delete testapp.py

--- blog/Deploying-Python-web-apps-with-nginx-and-Gunicorn/testapp.py
+++
@@ -1,9 +0,0 @@
-def testapp(environ, start_response):
-	data = 'Hello, World!\n'
-	status = '200 OK'
-	response_headers = [
-    	('Content-type','text/plain'),
-    	('Content-length',str(len(data)))
-	]
-	start_response(status, response_headers)
-	return iter([data])
